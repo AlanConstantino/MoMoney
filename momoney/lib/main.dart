d1877fabcf8998e19a4b76ac22b560cf5086fac2
bdupitas <brandon.dupitas.871@my.csun.edu>
2019-06-24 01:05:18 -0700

    Reworked home pages to be more receptive

--- momoney/lib/main.dart
+++ momoney/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(new MyApp(
+  ));
 }
 class MyApp extends StatelessWidget {
 
@@ -27,31 +28,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedPage = 0;
+  final _pageOptions = [
+    Text('Item 1'),
+    Text('Item 2'),
+    Text('Item 3'),
+  ];
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Welcome To MoMoney!'),
-      ),
-      bottomNavigationBar: new BottomNavigationBar(
+    return MaterialApp(
+      home: Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome To MoMoney!'),
+        ),
+        body: _pageOptions[_selectedPage],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedPage,
+          onTap: (int index) {
+            setState(() {
+              _selectedPage = index;
+            });
+          },
           items: [
             new BottomNavigationBarItem(
-              icon: const Icon(Icons.accessibility),
-              title: new Text('My Profile'),
+                icon: Icon(Icons.home),
+                title: Text("My Profile")
             ),
-
-            new BottomNavigationBarItem(
-              icon: const Icon(Icons.sentiment_satisfied),
-              title: new Text('Goals'),
+          new  BottomNavigationBarItem(
+                 icon: Icon(Icons.accessibility),
+                title: Text("My Plan")
             ),
-
             new BottomNavigationBarItem(
-              icon: const Icon(Icons.format_align_justify),
-              title: new Text('Settings'),
-            )
+                 icon: Icon(Icons.format_align_justify),
+                title: Text("My Settings")
+            ),
           ]
+        )
+    )
 
-      ),
     );
   }
 }
+
+
