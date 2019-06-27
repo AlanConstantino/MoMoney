e71b7f5dd54b0377d4b6b94547d1659ad060d23c
AlanConstantino <constantinoalan98@gmail.com>
2019-06-27 14:48:31 -0700

    Added database.dart and updated some fields in User.dart

--- momoney/lib/model/user.dart
+++ momoney/lib/model/user.dart
@@ -1,46 +1,44 @@
-// To parse this JSON data, do
-//
-//     final user = userFromJson(jsonString);
-
-import 'dart:convert';
-
-User userFromJson(String str) => User.fromJson(json.decode(str));
-
-String userToJson(User data) => json.encode(data.toJson());
-
 class User {
+  // attributes
   int id;
   String firstName;
   String lastName;
+  String goal;
   double monthlyIncome;
   double monthlyExpense;
 
+  // constructor
   User({
     this.id,
     this.firstName,
     this.lastName,
+    this.goal,
     this.monthlyIncome,
     this.monthlyExpense,
   });
 
-  factory User.fromJson(Map<String, dynamic> json) => new User(
+  // converting User map into object
+  factory User.fromMap(Map<String, dynamic> json) => new User(
         id: json["id"],
         firstName: json["first_name"],
         lastName: json["last_name"],
+        goal: json["goal"],
         monthlyIncome: json["monthly_income"],
         monthlyExpense: json["monthly_expense"],
       );
 
-  Map<String, dynamic> toJson() => {
+  // converting User object into map
+  Map<String, dynamic> toMap() => {
         "id": id,
         "first_name": firstName,
         "last_name": lastName,
+        "goal": goal,
         "monthly_income": monthlyIncome,
         "monthly_expense": monthlyExpense,
       };
 
   // only for debugging
-  save(){
+  save() {
     print('saving to database...');
   }
 }
