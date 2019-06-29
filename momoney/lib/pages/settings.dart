cb2fb1d962f29d927b1b4204bfa3734b5029b0ea
AlanConstantino <constantinoalan98@gmail.com>
2019-06-28 18:22:04 -0700

    Removed the dashboard bottom navigation bar and converted settings.dart into a stateful widget

--- momoney/lib/pages/settings.dart
+++ momoney/lib/pages/settings.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
+  Settings({Key key}) : super(key: key);
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("settings"),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        title: Text('Settings'),
+      ),
     );
   }
 }
