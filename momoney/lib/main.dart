5f26be7430bc91b7552776e3152bd482eb312ab2
AlanConstantino <constantinoalan98@gmail.com>
2019-06-29 05:23:21 -0700

    Restuructured files

--- momoney/lib/main.dart
+++ momoney/lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:momoney/pages/goals.dart';
+import 'package:momoney/pages/register.dart';
 import 'package:momoney/pages/dashboard.dart';
 import 'package:momoney/pages/forms/add_expense_form.dart';
 import 'package:momoney/pages/forms/add_income_form.dart';
-import 'package:momoney/pages/monthly_expenses.dart';
-import 'package:momoney/pages/goals.dart';
-import 'package:momoney/pages/register.dart';
-import 'package:momoney/pages/monthly_income.dart';
-import 'package:momoney/pages/settings.dart';
-import 'package:momoney/pages/profile.dart';
+import 'package:momoney/pages/drawer/profile.dart';
+import 'package:momoney/pages/drawer/settings.dart';
+import 'package:momoney/pages/drawer/monthly_expenses.dart';
+import 'package:momoney/pages/drawer/monthly_income.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: true,
+        debugShowCheckedModeBanner: false,
         title: 'MoMoney',
         theme: ThemeData(
           primarySwatch: Colors.blue,
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
           '/register': (_) => Register(),
           '/settings': (_) => Settings(),
           '/profile': (_) => Profile(),
-          '/goals': (_) => Goals(),
+          '/goals': (_) => Goals(), // not currently being used
           '/monthly_expenses': (_) => MonthlyExpenses(),
           '/monthly_income': (_) => MonthlyIncome(),
           '/add_income_form': (_) => AddIncomeForm(),
