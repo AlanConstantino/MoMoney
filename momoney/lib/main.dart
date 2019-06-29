31d7ea46e0cb80d1cb853896aa810a029550d87f
bdupitas <brandon.dupitas.871@my.csun.edu>
2019-06-28 19:24:33 -0700

    Drawer navigation
    Drawer navigation
    
    All buttons point to something, if the page is not built up it has sent to the dashboard,
    
    As of right now there is not a way to go back from a button press, as in each is a one way trip.
    
    There is a fix but it throws an error, I will update the trello.

--- momoney/lib/main.dart
+++ momoney/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:momoney/pages/unused_pages/signup.dart';
 import 'package:momoney/pages/dashboard.dart';
 import 'package:momoney/pages/register.dart';
 import 'package:momoney/pages/settings.dart';
+import 'package:momoney/pages/mygoals.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,11 +23,15 @@ class MyApp extends StatelessWidget {
           accentColor: const Color(0xFF2196f3),
           canvasColor: const Color(0xFFfafafa),
         ),
+
         home: Register(),
+
         routes: {
-          '/dashboard': (_) => Dashboard(),
-          '/register': (_) => Register(),
-          '/settings': (_) => Settings(),
+          '/dashboard': (BuildContext context) => new Dashboard(),
+          '/register': (BuildContext context) => new Register(),
+          '/settings': (BuildContext context) => new Settings(),
+          '/mygoals': (BuildContext context) => new MyGoals(),
+
 
           // may end up deleting the following later down the line
           '/login': (_) => Login(),
